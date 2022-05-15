66ba356c806dc50177199f288a2827ba47583a5f
SabinoCarlucci <94316189+SabinoCarlucci@users.noreply.github.com>
2022-05-15 19:40:22 +0200

    Update index.js
    made it better

--- Scrimba - learn javascript for free/basic unit converter/src/index.js
+++ Scrimba - learn javascript for free/basic unit converter/src/index.js
@@ -1,31 +1,23 @@
 let valoreDaConv = 0;
-let metersToFeet = (valoreDaConv * 3.28084).toFixed(3);
-let feetToMeters = (valoreDaConv * 0.3048).toFixed(3);
-let litersToGallons = (valoreDaConv * 0.2641722).toFixed(3);
-let gallonsToLiters = (valoreDaConv * 3.785411784).toFixed(3);
-let kilosToPounds = (valoreDaConv * 2.20462).toFixed(3);
-let poundsToKilos = (valoreDaConv * 0.453592).toFixed(3);
+convert(valoreDaConv)
 
-document.getElementById("numero").innerHTML= valoreDaConv
-
-document.getElementById("p1").innerHTML= `${valoreDaConv} meters = ${metersToFeet} feet | ${valoreDaConv} feet = ${feetToMeters} meters`
-
-document.getElementById("p2").innerHTML= `${valoreDaConv} liters = ${litersToGallons} gallons | ${valoreDaConv} gallons = ${gallonsToLiters} liters`
-
-document.getElementById("p3").innerHTML= `${valoreDaConv} kilos = ${kilosToPounds} pounds | ${valoreDaConv} pounds = ${poundsToKilos} kilos`
-
-let submitBtn = document.getElementById('submitBtn')
-
-submitBtn.addEventListener("click", function() {    
-    let valoreDaConv = document.getElementById('valueToConvert').value
+function convert(valoreDaConv){
     let metersToFeet = (valoreDaConv * 3.28084).toFixed(3);
     let feetToMeters = (valoreDaConv * 0.3048).toFixed(3);
     let litersToGallons = (valoreDaConv * 0.2641722).toFixed(3);
     let gallonsToLiters = (valoreDaConv * 3.785411784).toFixed(3);
     let kilosToPounds = (valoreDaConv * 2.20462).toFixed(3);
-    let poundsToKilos = (valoreDaConv * 0.453592).toFixed(3);    
+    let poundsToKilos = (valoreDaConv * 0.453592).toFixed(3);
     document.getElementById("numero").innerHTML= valoreDaConv
     document.getElementById("p1").innerHTML= `${valoreDaConv} meters = ${metersToFeet} feet | ${valoreDaConv} feet = ${feetToMeters} meters`
     document.getElementById("p2").innerHTML= `${valoreDaConv} liters = ${litersToGallons} gallons | ${valoreDaConv} gallons = ${gallonsToLiters} liters`
     document.getElementById("p3").innerHTML= `${valoreDaConv} kilos = ${kilosToPounds} pounds | ${valoreDaConv} pounds = ${poundsToKilos} kilos`
+}
+
+let submitBtn = document.getElementById('submitBtn')
+
+submitBtn.addEventListener("click", function() {    
+    let valoreDaConv = document.getElementById('valueToConvert').value
+    convert(valoreDaConv)
+    document.getElementById('valueToConvert').value = ""
 })
